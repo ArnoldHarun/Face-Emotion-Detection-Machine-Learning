b0b4f28afa3d5f54a6e1b3434737b5d1b35d776e
ArnoldHarun <harunk3570@gmail.com>
2024-03-15 11:27:11 +0300

    added pd dataframe
    with '#' will be ignored, and an empty message aborts the commit.

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,27 @@
 import logging
 import pandas as pd
+import numpy as np 
+import os
+import seaborn as sn 
+import tensorflow as tf 
+from keras.models import Sequential 
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from zenml import step
 
+TRAIN_DIR ='C:\Users\User\Desktop\Project\Data\train'
+TEST_DIR='C:\Users\User\Desktop\Project\Data\test'
+
 @step
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+def load_dataset(directory):
+    image_paths = []
+    labels=[]
+    
+    for label in os.listdir(directory):
+        for filename in os.listdir(directory+label):
+            image_path = os.path.join(directory, label, filename)
+            image_paths.append(image_path)
+            labels.append(label)
+    print(label, 'Completed')
+    return image_paths, labels
+    
